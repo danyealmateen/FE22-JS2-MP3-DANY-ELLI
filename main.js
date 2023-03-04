cd0d9ba62a338b3eee5b0ef9c62ae12682cd344a
Danyeal <95497503+danyealmateen@users.noreply.github.com>
2023-03-04 15:07:01 +0100

    fb renderat på DOM

---
+++ main.js
@@ -0,0 +1,65 @@
+const url = `https://della-311b1-default-rtdb.europe-west1.firebasedatabase.app/.json`;
+const productCard = document.getElementById("productCard");
+const candyCard = document.getElementById("candyCard");
+const chipsCard = document.getElementById("chipsCard");
+const cookieCard = document.getElementById("cookieCard");
+const gumCard = document.getElementById("gumCard");
+const sodaCard = document.getElementById("sodaCard");
+
+//GetProducts
+async function getProducts() {
+  const response = await fetch(url);
+  const data = await response.json();
+  let arrayObj = Object.values(data);
+
+  //Candy (Skittles)
+  arrayObj.forEach((obj) => {
+    const { name, price, stock, url } = obj.candy;
+    candyCard.innerHTML = `
+        <img src="${url}"/><br>
+        Name:${name}<br>
+        Price:${price}<br> 
+        Stock:${stock}<br>`;
+  });
+
+  //Chips (Estrella)
+  arrayObj.forEach((obj) => {
+    const { name, price, stock, url } = obj.chips;
+    chipsCard.innerHTML = `
+        <img src="${url}"/><br>
+        Name:${name}<br>
+        Price:${price}<br> 
+        Stock:${stock}<br>`;
+  });
+
+  //Cookie (Marabou)
+  arrayObj.forEach((obj) => {
+    const { name, price, stock, url } = obj.cookie;
+    cookieCard.innerHTML = `
+        <img src="${url}"/><br>
+        Name:${name}<br>
+        Price:${price}<br> 
+        Stock:${stock}<br>`;
+  });
+
+  //Gum (Stimerol)
+  arrayObj.forEach((obj) => {
+    const { name, price, stock, url } = obj.gum;
+    gumCard.innerHTML = `
+        <img src="${url}"/><br>
+        Name:${name}<br>
+        Price:${price}<br> 
+        Stock:${stock}<br>`;
+  });
+
+  //Soda (Pepsi)
+  arrayObj.forEach((obj) => {
+    const { name, price, stock, url } = obj.soda;
+    sodaCard.innerHTML = `
+            <img src="${url}"/><br>
+            Name:${name}<br>
+            Price:${price}<br> 
+            Stock:${stock}<br>`;
+  });
+}
+getProducts();
