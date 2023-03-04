944779b74918e60937713a5ff6c4de76df1ab34a
Danyeal <95497503+danyealmateen@users.noreply.github.com>
2023-03-04 15:22:41 +0100

    eventlisteners in place

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const url = `https://della-311b1-default-rtdb.europe-west1.firebasedatabase.app/.json`;
+let itemsInCart = 0;
+
+//Cards
 const productCard = document.getElementById("productCard");
 const candyCard = document.getElementById("candyCard");
 const chipsCard = document.getElementById("chipsCard");
 const cookieCard = document.getElementById("cookieCard");
 const gumCard = document.getElementById("gumCard");
 const sodaCard = document.getElementById("sodaCard");
+
+//Buttons
+const candyBtn = document.getElementById("candyBtn");
+const chipsBtn = document.getElementById("chipsBtn");
+const cookieBtn = document.getElementById("cookieBtn");
+const gumBtn = document.getElementById("gumBtn");
+const sodaBtn = document.getElementById("sodaBtn");
 
 //GetProducts
 async function getProducts() {
@@ -60,6 +70,27 @@ async function getProducts() {
             Name:${name}<br>
             Price:${price}<br> 
             Stock:${stock}<br>`;
+  });
+
+  //Eventlisteners
+  candyBtn.addEventListener("click", () => {
+    itemsInCart++;
+  });
+
+  chipsBtn.addEventListener("click", () => {
+    itemsInCart++;
+  });
+
+  cookieBtn.addEventListener("click", () => {
+    itemsInCart++;
+  });
+
+  gumBtn.addEventListener("click", () => {
+    itemsInCart++;
+  });
+
+  sodaBtn.addEventListener("click", () => {
+    itemsInCart++;
   });
 }
 getProducts();
