3bf27ba685c02058d2c4c66c3170b53737c67c0b
Danyeal <95497503+danyealmateen@users.noreply.github.com>
2023-03-04 17:15:46 +0100

    asdasdasd

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 const url = `https://della-311b1-default-rtdb.europe-west1.firebasedatabase.app/.json`;
+let productArray = [];
+postCart();
+getProducts();
 let itemsInCart = 0;
-let cart = [];
+
+//Products
+let candy = { name: "skittles", price: 10, stock: 1, url: "blabla", id: 1 };
+let chips = { name: "estrella", price: 15, stock: 2, url: "blabla", id: 2 };
+let cookie = { name: "marabou", price: 20, stock: 3, url: "blabla", id: 3 };
+let gum = { name: "stimerol", price: 25, stock: 4, url: "blabla", id: 4 };
+let soda = { name: "pepsi", price: 30, stock: 5, url: "blabla", id: 5 };
+
+//Cards pushed into the array
+productArray.push(candy);
+productArray.push(chips);
+productArray.push(cookie);
+productArray.push(gum);
+productArray.push(soda);
 
 //Cards
 const itemCounter = document.getElementById("itemCounter");
@@ -18,61 +34,46 @@ const cookieBtn = document.getElementById("cookieBtn");
 const gumBtn = document.getElementById("gumBtn");
 const sodaBtn = document.getElementById("sodaBtn");
 
+candyCard.innerHTML = `
+${productArray[0].name}<br>
+${productArray[0].price}<br>
+${productArray[0].stock}<br>
+${productArray[0].url}<br>
+`;
+
+chipsCard.innerHTML = `
+${productArray[1].name}<br>
+${productArray[1].price}<br>
+${productArray[1].stock}<br>
+${productArray[1].url}<br>
+`;
+
+cookieCard.innerHTML = `
+${productArray[2].name}<br>
+${productArray[2].price}<br>
+${productArray[2].stock}<br>
+${productArray[2].url}<br>
+`;
+
+gumCard.innerHTML = `
+${productArray[3].name}<br>
+${productArray[3].price}<br>
+${productArray[3].stock}<br>
+${productArray[3].url}<br>
+`;
+
+sodaCard.innerHTML = `
+${productArray[4].name}<br>
+${productArray[4].price}<br>
+${productArray[4].stock}<br>
+${productArray[4].url}<br>
+`;
+
 //GetProducts
 async function getProducts() {
   const response = await fetch(url);
   const data = await response.json();
-  let arrayObj = Object.values(data);
-
-  //Candy (Skittles)
-  arrayObj.forEach((obj) => {
-    const { name, price, stock, url } = obj.candy;
-    candyCard.innerHTML = `
-        <img src="${url}"/><br>
-        Name:${name}<br>
-        Price:${price}<br> 
-        Stock:${stock}<br>`;
-  });
-
-  //Chips (Estrella)
-  arrayObj.forEach((obj) => {
-    const { name, price, stock, url } = obj.chips;
-    chipsCard.innerHTML = `
-        <img src="${url}"/><br>
-        Name:${name}<br>
-        Price:${price}<br> 
-        Stock:${stock}<br>`;
-  });
-
-  //Cookie (Marabou)
-  arrayObj.forEach((obj) => {
-    const { name, price, stock, url } = obj.cookie;
-    cookieCard.innerHTML = `
-        <img src="${url}"/><br>
-        Name:${name}<br>
-        Price:${price}<br> 
-        Stock:${stock}<br>`;
-  });
-
-  //Gum (Stimerol)
-  arrayObj.forEach((obj) => {
-    const { name, price, stock, url } = obj.gum;
-    gumCard.innerHTML = `
-        <img src="${url}"/><br>
-        Name:${name}<br>
-        Price:${price}<br> 
-        Stock:${stock}<br>`;
-  });
-
-  //Soda (Pepsi)
-  arrayObj.forEach((obj) => {
-    const { name, price, stock, url } = obj.soda;
-    sodaCard.innerHTML = `
-            <img src="${url}"/><br>
-            Name:${name}<br>
-            Price:${price}<br> 
-            Stock:${stock}<br>`;
-  });
+  console.log(data);
 
   //Eventlisteners
   candyBtn.addEventListener("click", () => {
@@ -110,4 +111,17 @@ async function getProducts() {
     console.log(cart);
   });
 }
-getProducts();
+
+// // Post the cart with snacks to FB
+async function postCart() {
+  const init = {
+    method: "PUT",
+    body: JSON.stringify(productArray),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+
+  const response = await fetch(url, init);
+  const data = await response.json();
+}
