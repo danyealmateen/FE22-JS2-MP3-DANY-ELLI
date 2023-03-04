6a1e5612b9ab3b13dea1647299ebbb903bf46cf0
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2023-03-04 21:35:54 +0100

    tog bort kod från kundvagn.js

--- js/kundvagn.js
+++ js/kundvagn.js
@@ -25,35 +25,3 @@ if (cartItems !== null) {
   });
 }
 
-async function getProducts() {
-  const response = await fetch(url);
-  const data = await response.json();
-  const shoppingCart = document.getElementById("shoppingCart");
-  let cartItems = JSON.parse(localStorage.getItem("cartItems"));
-  let totalPrice = 0;
-
-  if (cartItems !== null && cartItems.length > 0) {
-    cartItems.forEach((item) => {
-      const product = data.find((p) => p.name === item.name);
-      if (product !== undefined) {
-        const itemPrice = product.price * item.quantity;
-        totalPrice += itemPrice;
-        const itemElement = document.createElement("div");
-        itemElement.classList.add("cart-item");
-        itemElement.setAttribute("data-product", product.name);
-        itemElement.setAttribute("data-price", product.price);
-        itemElement.setAttribute("data-quantity", item.quantity);
-        itemElement.innerHTML = `
-          <p>${product.name} x ${item.quantity} - $${itemPrice}</p>
-          <button class="remove-item-btn">Remove</button>
-        `;
-        shoppingCart.appendChild(itemElement);
-      }
-    });
-  }
-
-  const totalElement = document.createElement("p");
-  totalElement.innerHTML = `Total: $${totalPrice}`;
-  shoppingCart.appendChild(totalElement);
-}
-
